a3ef62a84c67a90587d89039a63ce4421e246a01
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2022-02-28 14:32:07 +0100

    clean

---
+++ Assets/Scripts/UI/Loading/LoadingManager.cs
@@ -0,0 +1,18 @@
+﻿using MoreMountains.Tools;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Loading
+{
+	public class LoadingManager : MMSceneLoadingManager 
+	{
+		protected override void Start()
+		{
+			// no progress bar
+			var g =new GameObject();
+			LoadingProgressBar = g.AddComponent<CanvasGroup>();
+			LoadingProgressBar.gameObject.AddComponent<Image>();
+			base.Start();
+		}
+	}
+}

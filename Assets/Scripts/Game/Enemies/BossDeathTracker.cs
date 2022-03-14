b46a486b0eca82c02b827cdf456053a20ba6143e
ElRaphik <raphael.soranzo@gmail.com>
2022-03-14 05:42:23 +0100

    Killing boss should give achievement

---
+++ Assets/Scripts/Game/Enemies/BossDeathTracker.cs
@@ -0,0 +1,16 @@
+﻿using Game.Events;
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+namespace Game.Enemies
+{
+    [RequireComponent(typeof(Health))]
+    public class BossDeathTracker : MonoBehaviour
+    {
+        private void Awake()
+        {
+            // every enemy tracked will trigger this event
+            GetComponent<Health>().OnDeath = RHEvent.TriggerBossDeath;
+        }
+    }
+}
